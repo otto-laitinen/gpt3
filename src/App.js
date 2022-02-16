9900ff96e836ad7e523515d19eddef394e9209c6
Otto <ottolaitinen@outlook.com>
2022-02-16 18:57:09 +0200

    add basic css styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+// The following 2 import lines are coming from separate index.js files, specifically made for cleaner looking importing
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
 const App = () => {
   return (
-    <div><h1>GPT-3</h1></div>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
